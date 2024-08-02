fcc15e8a4adf47d0b73ad87e4308e4bfcb93e00d
chheng <chheng@gmail.com>
2024-08-02 14:44:26 +0700

    fix bug

--- babel.config.js
+++ babel.config.js
@@ -5,11 +5,7 @@ module.exports = function (api) {
     plugins: [
       "nativewind/babel",
       "react-native-worklets-core/plugin",
-      
-        "react-native-reanimated/plugin",
-        {
-          globals: ["__labelImage"],
-        },
+    
       
     ],
   };
